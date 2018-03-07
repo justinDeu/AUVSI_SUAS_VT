ca0722b6f3927c0655936b1656a4dced6f799b57
Justin <jusdeutsch22@gmail.com>
2018-03-07 17:19:38 -0500

    Added simple parsing of url, username, and password for interop_parser

--- interop/client/interop_parser.py
+++ interop/client/interop_parser.py
@@ -1,8 +1,9 @@
 import interop as interop
+import argparse as argparse
 
 
-def connect_to_client(givenUser, givenPass):
-    client = interop.Client(url='http://127.0.0.1:8000',
+def connect_to_client(givenURL, givenUser, givenPass):
+    client = interop.Client(url=givenURL,
                           username=givenUser,
                           password=givenPass)
 
@@ -13,7 +14,19 @@ def connect_to_client(givenUser, givenPass):
 
 
 def main():
-    connect_to_client("testuser", "testpass")
+
+    parser = argparse.ArgumentParser(description="Interop Server Command Line Interface")
+
+    parser.add_argument("--url", required=True,
+                        help="URL for Server")
+    parser.add_argument("--username", required=True,
+                        help="Username for Account")
+    parser.add_argument("--password", required=True,
+                        help="Password for Account")
+
+    args = parser.parse_args()
+
+    connect_to_client(args.url, args.username, args.password)
 
 
 if __name__ == "__main__":
